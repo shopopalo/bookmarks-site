e02eed9e68c57f9317d782f4a28d8b9485b40173
phonxis <phonxis@gmail.com>
2016-04-05 17:56:38 +0300

    initial

---
+++ account/authentication.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+
+
+class EmailAuthBackend(object):
+	"""
+	Authentication using e-mail account
+	"""
+	def authenticate(self, username=None, password=None):
+		try:
+			user = User.objects.get(email=username)
+			if user.check_password(password):
+				return user
+			return None
+		except User.DoesNotExist:
+			return None
+
+	def get_user(self, user_id):
+		try:
+			user = User.objects.get(pk=user_id)
+			if user.is_active:
+				return user
+		except User.DoesNotExist:
+			return None
---
+++ images/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import JsonResponse
+from .forms import ImageCreateForm
+from .models import Image
+
+
+# Create your views here.
+
+
+@login_required
+def image_create(request):
+	if request.method == 'POST':
+		form = ImageCreateForm(data=request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			new_item = form.save(commit=False)
+
+			# add current user to item
+			new_item.user = request.user
+			new_item.save()
+			messages.success(request, 'Зображення успішно додано')
+
+			return redirect(new_item.get_absolute_url())
+	else:
+		form = ImageCreateForm(data=request.GET)
+
+	return render(request,
+					'images/image/create.html',
+					{'section': 'images',
+					'form': form})
+
+def image_detail(request, id, slug):
+	image = get_object_or_404(Image, id=id, slug=slug)
+	return render(request,
+					'images/image/detail.html',
+					{'section': 'images',
+					'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+	image_id = request.POST.get('id')		# the id image on which performing action
+	action = request.POST.get('action')		# action which user wants to perform - like/unlike
+	if image_id and action:
+		try:
+			image = Image.objects.get(id=image_id)
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			return JsonResponse({'status': 'ok'})
+		except:
+			pass
+	return JsonResponse({'status': 'do not ok'})
+
